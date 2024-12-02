23489d23a924a95a03b6346563443837b937bf1c
S.S.M.Chathuranganee <sandunichathurangi18@gmail.com>
2024-12-03 04:35:50 +0530

    export isAdiminValid function

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -52,7 +52,80 @@ export function loginUser(req,res){
     }
   )
 }
+//------Sign In Function---------
+export function loginUsers(req,res){          
+  const credentials = req.body
 
+  User.findOne({email: credentials.email}).then(
+      (user)=>{
+          if(!user){
+              res.status(403).json({
+                  message: "User not found"
+              })
+          }else{
+              const isPasswordValid = bcrypt.compareSync(credentials.password, user.password)
+              
+              if(!isPasswordValid){
+                  res.status(403).json({
+                      message: "Incorrect Password"
+                  })
+              }else{
+                  const payload = {
+                      id: user._id,
+                      email: user.email,
+                      firstName: user.firstName,
+                      lastName: user.lastName,
+                      type: user.type
+                  };
+
+                  const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: "48h"});   //asign user information to the "token" , "secret" is the password
+              
+                  res.json({
+                      message: "User found",
+                      user: user,
+                      token: token   //send the token to the frontend
+                  })
+              }
+          }
+      }
+  )
+}
+
+//-----Admin checking------------
+export function isAdminValid(req){
+  if(req.user == null){
+      return false
+  }
+  if(req.user.type != "Admin"){
+      return false
+  }
+  return true
+}
+
+//-----Customer checking-----------
+export function isCustomerValid(req){
+  if(req.user == null){
+      return false
+  }
+  if(req.user.type != "Customer"){
+      return false
+  }
+  return true
+}
+
+export function getUser(req, res){
+  const user = req.body.user
+  if(user == null){
+      res.json({
+          message: "not found"
+      })
+  }else{
+      res.json({
+          message: "found",
+          user: user
+      })
+  }
+}
         
 
 
