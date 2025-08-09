38bf758422c92e5867c683aafb7d23391ca3e22d
S.S.M.Chathuranganee <sandunichathurangi18@gmail.com>
2025-08-09 13:46:36 +0530

    update controllers

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,49 +1,128 @@
-import Booking from "../models/booking.js";
-import { isCustomerValid } from "./userControllers.js";
+import Category from "../models/category.js";
+import { isAdminValid } from "./userControllers.js";
 
-//-------------------New Booking Function--------------------
-export function createBooking(req, res){
-
-    if(!isCustomerValid(req)){
+//----------------------------Add new Category-------------------------
+export function createCategory(req, res){
+    if(req.body.user == null){
+        res.status(401).json({
+            message: "Unauthorized"
+        })
+        return
+    }
+    if(req.body.user.type != "Admin"){
         res.status(403).json({
             message: "Forbidden"
         })
         return
     }
-
-    const startingId = 1200;
-
-    Booking.countDocuments({}).then(
-        (count)=>{
-            const newId = startingId + count + 1
-            const newBooking = new Booking({
-                bookingId: newId,
-                roomId: req.body.roomId,
-                email: req.user.email,
-                start: req.body.start,
-                end: req.body.end
+    const newCategory = new Category(req.body)
+    newCategory.save().then(
+        (result)=>{
+            res.json({
+                message: "Category created succesfully",
+                result: result
             })
-            newBooking.save().then(
-                (result)=>{
-                    res.json({
-                        message: "Booking Created Successfully",
-                        result: result
-                    })
-                }
-            ).catch(
-                (err)=>{
-                    res.json({
-                        message: "Booking Creation Failed",
-                        error: err
-                    })
-                }
-            )
         }
     ).catch(
         (err)=>{
             res.json({
-                message: "Booking Creation Failed",
+                message: "Category creation failed",
                 error: err
+            })
+        }
+    )
+}
+
+//------------------------------Delete Category--------------------------------
+export function deleteCategory(req, res){
+    if(req.body.user == null){
+        res.status(401).json({
+            message: "Unauthorized"
+        })
+        return
+    }
+    if(req.body.user.type != "Admin"){
+        res.status(403).json({
+            message: "Forbidden"
+        })
+        return
+    }
+    const name = req.params.name
+    Category.findOneAndDelete({name: name}).then(
+        ()=>{
+            res.json({
+                message: "Category Deleted Successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message: "Category Deletion Failed"
+            })
+        }
+    )
+}
+
+//---------------------------Show All Categories------------------------------
+export function getCategory(req, res){
+    Category.find().then(
+        (result)=>{
+            res.json({
+                categories: result
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message: "Failed to get Categories"
+            })
+        }
+    )
+}
+
+//---------------Get Category Details by it's Name--------------------
+export function getCategoryByName(req, res){
+    const name = req.params.name
+    Category.findOne({name: name}).then(
+        (result)=>{
+            if(result == null){
+                res.json({
+                    message: "Category not found"
+                })
+            }else{
+                res.json({
+                    category: result
+                })
+            }
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                message: "Failed to get Category"
+            })
+        }
+    )
+}
+
+//------------------------Update Category------------------------------
+export function updateCategory(req, res){
+    if(!isAdminValid(req)){
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+        return
+    }
+    const name = req.params.name
+    Category.updateOne({name: name},req.body).then(
+        ()=>{
+            res.json({
+                message: "Category Updated Successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.json({
+                massage: "Failed to Update Category"
             })
         }
     )
--- controllers/galleryItemController.js
+++ controllers/galleryItemController.js
@@ -1,41 +1,112 @@
 import GalleryItem from "../models/galleryItem.js"
 
-export function createGalleryItem(req,res){
+//-------------------------Add new Gallery item------------------------------
+export function createGalleryItem(req, res){   
+   
+    const user = req.body.user                           //Check the Access
+    if(!user){
+        res.status(403).json({
+            message: "Please login to create Gallery Item"
+        })
+        return
+    }
+    if(user.type != "Admin"){
+        res.status(403).json({
+            message: "You do not have permission to create a Gallery Item"
+        })
+        return
+    }
 
-  const user = req.body.user
+    const galleryItem = req.body                    //Create new Gallery item
+    const newGalleryItem = new GalleryItem(galleryItem)
+    newGalleryItem.save().then(
+        ()=>{
+            res.json({
+                message: "Gallery item created successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.status(500).json({
+                message: "Gallery item creation failed"
+            })
+        }
+    )
+}
 
-  if(user == null){
-    res.status(403).json({
-      message : "Please login to create a gallery item"
-    })
-    return
+//-----------------------Show all gallery itmes in front end------------------------
+export function getGalleryItems(req,res){           
+    GalleryItem.find().then(
+        (list)=>{
+            res.json({
+                list: list
+            })
+        }
+    )
+}
+
+//--------------------------Delete Gallery Item-----------------------------
+export function deleteGalleryItem(req, res){
+    const id = req.params.id
+    const user = req.body.user                           //Check the Access
+    if(!user){
+        res.status(403).json({
+            message: "Please login to delete Gallery Item"
+        })
+        return
+    }
+    if(user.type != "Admin"){
+        res.status(403).json({
+            message: "You do not have permission to delete a Gallery Item"
+        })
+        return
+    } 
+
+    GalleryItem.findByIdAndDelete(id).then(
+        ()=>{
+            res.json({
+                message: "Gallery Item Deleted Successfully"
+            })
+        }
+    ).catch(
+        ()=>{
+            res.status(500).json({
+                message: "Galley item deletion failed"
+            })
+        }
+    )
+}
+
+//------------------------Update Gallery Item------------------------------
+export function updateGalleryItem(req,res){
+    const id = req.params.id
+    const user = req.body.user                           //Check the Access
+    if(!user){
+        res.status(403).json({
+            message: "Please login to delete Gallery Item"
+        })
+        return
+    }
+    if(user.type != "Admin"){
+        res.status(403).json({
+            message: "You do not have permission to delete a Gallery Item"
+        })
+        return
+    } 
+  
+    const galleryItem = req.body
+  
+    GalleryItem.findByIdAndUpdate(id,galleryItem).then(
+      ()=>{
+        res.json({
+          message : "Gallery Item updated successfully"
+        })
+      }
+    ).catch(
+      ()=>{
+        res.status(500).json({
+          message : "Gallery Item update failed"
+        })
+      }
+    )
   }
-
-  const galleryItem = req.body.item
-
-  const newGalleryItem = new GalleryItem(galleryItem)
-  newGalleryItem.save().then(
-    ()=>{
-      res.json({
-        message : "Gallery Item created successfully"
-      })
-    }
-  ).catch(
-    ()=>{
-      res.status(500).json({
-        message : "Gallery Item creation failed"
-      })
-    }
-  )
-}
-
-export function getGalleryItems(req,res){
-  GalleryItem.find().then(
-    (list)=>{
-      res.json({
-        list : list
-      })
-    }
-  )
-}
-
