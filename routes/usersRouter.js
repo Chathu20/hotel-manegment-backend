463695953adf803b92726c03751d29746e25e7d1
S.S.M.Chathuranganee <sandunichathurangi18@gmail.com>
2025-08-09 13:42:32 +0530

    update

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,15 +1,18 @@
-import express from 'express'
-import {  loginUser, postUsers,getUser  } from '../controllers/userControllers.js'
+import express from "express";
+import { postUsers, loginUsers, getUsers, updateUser, deleteUser, getUser } from "../controllers/userControllers.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/",postUsers)
 
-userRouter.post("/login",loginUser)
+userRouter.post("/login",loginUsers)
 
-userRouter.get("/",getUser)
+userRouter.get("/", getUser)
 
+userRouter.get("/all",getUsers)
 
+userRouter.put("/:email", updateUser)
 
+userRouter.delete("/:email", deleteUser)
 
 export default userRouter;
